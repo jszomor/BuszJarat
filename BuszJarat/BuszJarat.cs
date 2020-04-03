a72f59224dbdeaa99413a7b6b2e852a49faa9ad8
Szomor János <jszomor@gmail.com>
2020-04-03 18:00:55 +0200

    TimeInterval DistanceCalculator BuszPozicio

--- BuszJarat/BuszJarat.cs
+++ BuszJarat/BuszJarat.cs
@@ -15,20 +15,18 @@ namespace BuszJarat
       public string MegalloNeve;
       public int Tavolsag;
     }
-    public string Pozicio;
+    public int Pozicio = 0;
     public string startDate;
     public Entries[] getEntries = new Entries[100];
     public int Counter = 1;
+    public double megtettUt;
 
     public BuszJarat(string file)
     {
       StreamReader fileReader = new StreamReader(file);
       string sor;
       string[] adatok;
-      //string startDate = File.ReadAllLines()
-      //  fileReader.ReadLine()
       startDate = fileReader.ReadLine();
-
       while ((sor = fileReader.ReadLine()) != null)
       {
         adatok = sor.Split('\t');
@@ -39,29 +37,49 @@ namespace BuszJarat
         getEntries[Counter].Tavolsag = int.Parse(adatok[2]);
         Counter++;
       }
-
     }
     public void Print()
     {
-      Console.WriteLine(startDate);
-      for (int i = 0; i < Counter; i++)
+      Console.WriteLine();
+      Console.WriteLine("Sorszám   Buszmegálló     Távolság(Km)");
+      for (int i = 1; i < Counter; i++)
       {
         Console.WriteLine("{0,3} {1,-20} {2,3}", getEntries[i].Id, getEntries[i].MegalloNeve, getEntries[i].Tavolsag);
       }
+      Console.WriteLine();
+      Console.WriteLine($"A busz ezen megállók között tartozkodik: {getEntries[Pozicio-1].MegalloNeve} - {getEntries[Pozicio].MegalloNeve}");
     }
-    public void TimeInterval(string dateString)
+    public TimeSpan TimeInterval(string arrival)
     {
-      //DateTime departure = new DateTime(start);
-      dateString = "7/10/1974 7:10:24 AM";
-      DateTime dateFromString = DateTime.Parse(dateString, System.Globalization.CultureInfo.InvariantCulture);
-      Console.WriteLine(dateFromString.ToString());
-      DateTime departure = new DateTime(2010, 6, 12, 18, 32, 0);
-      DateTime arrival = new DateTime(2010, 6, 13, 22, 47, 0);
-      TimeSpan travelTime = arrival - departure;
-      Console.WriteLine("{0} - {1} = {2}", arrival, departure, travelTime);
+      DateTime dateFromString = DateTime.Parse(startDate, System.Globalization.CultureInfo.InvariantCulture);
+      DateTime arrivalDate = DateTime.Parse(arrival, System.Globalization.CultureInfo.InvariantCulture);
+      TimeSpan travelTime = arrivalDate - dateFromString;
+      return travelTime;
+    }
+    public double DistanceCalculator(TimeSpan menetido, double sebesseg)
+    {
+      megtettUt = sebesseg * 1000 / 60 * menetido.TotalMinutes;
+      return megtettUt;
+    }
+    public void BuszPozicio()
+    {
+      bool megMegy = true;
+      double megtettKm = megtettUt / 1000;
+      while (megMegy)
+      {
+        for (int i = 1; i < Counter; i++)
+        {
+          if (megtettKm > 0)
+            megtettKm -= getEntries[i].Tavolsag;
 
-      // The example displays the following output:
-      //       6/13/2010 10:47:00 PM - 6/12/2010 6:32:00 PM = 1.04:15:00
+          else
+          {
+            Pozicio = i;
+            megMegy = false;
+            break;
+          }
+        }
+      }
     }
   }
 }
--- BuszJarat/Program.cs
+++ BuszJarat/Program.cs
@@ -12,9 +12,23 @@ namespace BuszJarat
     {
       string file = "21esBusz.txt";
       BuszJarat buszJarat = new BuszJarat(file);
+      Console.WriteLine("A program a busz indulási idejétől a felhasználó által megadott \n ideig eltelt idő alatt megtett helyét számolja ki a busznak.");
+      Console.WriteLine();
+      Console.WriteLine($"A busz indulási indőpontja: {buszJarat.startDate}");
+      Console.WriteLine("Adja meg a vizsgálni kívánt időpontot. Érvényes formátum:hónap.nap.év óra:perc:másodperc AM vagy PM");
+      string vizsgaltIdopont = Console.ReadLine();
+      TimeSpan menetido = buszJarat.TimeInterval(vizsgaltIdopont);
+      Console.WriteLine($"Az eddig eltelt menetidő: {menetido}");
+      Console.WriteLine("Adja meg a busz átlagos sebességét (Km/h)");
+      double sebesseg = int.Parse(Console.ReadLine());
+      Console.WriteLine($"Megtett út méterben: {buszJarat.DistanceCalculator(menetido, sebesseg)}m");
+      Console.WriteLine($"Megtett út kilóméterben: {(buszJarat.DistanceCalculator(menetido, sebesseg))/1000}km");
+      buszJarat.BuszPozicio();
+      Console.WriteLine();
+      Console.WriteLine("Megállók:");
       buszJarat.Print();
-      //buszJarat.TimeInterval();
       Console.ReadKey();
     }
   }
 }
+ 
